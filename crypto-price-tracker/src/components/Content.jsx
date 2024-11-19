4c2b3eb7f52d37b88855396f626d68a3e346cf5e
samue1eun <samuel.eun.park@gmail.com>
2024-11-18 22:52:33 -0600

    added cards w/o functionality

--- crypto-price-tracker/src/components/Content.jsx
+++ crypto-price-tracker/src/components/Content.jsx
@@ -1,14 +1,44 @@
-import React from "react";
-import { useState } from "react";
-
-// This will get all the contents from the API to put on a card
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Content = () => {
+    const [assets, setAssets] = useState([]);
+    const [topTenAssets, setTopTenAssets] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://api.coincap.io/v2/assets');
+                const data = response.data.data;
+                const topTenAssets = data.slice(0, 10).map((asset) => ({
+                    id: asset.id,
+                    rank: asset.rank,
+                    name: asset.name,
+                    price: parseFloat(asset.priceUsd).toFixed(4),
+                    changePercent24Hr: parseFloat(asset.changePercent24Hr).toFixed(2),
+                }))
+                const assets = data.map((asset) => ({
+                    rank: asset.rank,
+                    name: asset.name,
+                    price: parseFloat(asset.priceUsd).toFixed(4),
+                    changePercent24Hr: parseFloat(asset.changePercent24Hr).toFixed(2),
+                }))
+                setTopTenAssets(topTenAssets);
+            } catch (error) {
+                console.log('Error fetchin data:', error);
+            }
+    };
+    fetchData();
+    }, []);
+    
     return (
         <>
-
+        {topTenAssets.map((asset) => (
+            <li key={asset.id}>
+                Rank - {asset.rank}: {asset.name} - Price: ${asset.price} - Change 24hr: {asset.changePercent24Hr}%
+            </li>
+        ))}
         </>
     )
 }
-
-export default Content
+export default Content;
